ea002145dd3d809f27778a54bef6a327ee9b8888
Ekramul <mdekramulhassan168@gmail.com>
2023-09-22 10:48:04 +0600

    all setup added

---
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -0,0 +1,10 @@
+
+const AppliedJob = () => {
+    return (
+        <div>
+            <h1>Jobs i applied</h1>
+        </div>
+    );
+};
+
+export default AppliedJob;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './components/Root/Root';
+import Home from './components/Home/Home';
+import AppliedJob from './components/AppliedJob/AppliedJob';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/applied',
+        element: <AppliedJob></AppliedJob>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
